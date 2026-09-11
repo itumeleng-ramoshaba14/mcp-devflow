5425b31363b28eff1c3c897dbc8a984dac9c0db8
itumeleng-ramoshaba14 <itumeleng.maake.ramoshaba@gmail.com>
2026-09-11 18:10:44 +0200

    chore: initial MCP DevFlow project

---
+++ src/main/java/com/ramoshaba/devflow/McpDevflowApplication.java
@@ -0,0 +1,13 @@
+package com.ramoshaba.devflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpDevflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpDevflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramoshaba/devflow/McpDevflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramoshaba.devflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpDevflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
